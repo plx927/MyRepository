3003ca0ea6a15389e8a46db5cb1b166c48a5aaf2
plx <784580872@qq.com>
2016-04-12 09:46:30 +0800

    .

--- netty-discard/src/main/java/com/panlingxiao/netty/discard/DiscardServer.java
+++ netty-discard/src/main/java/com/panlingxiao/netty/discard/DiscardServer.java
@@ -5,6 +5,9 @@ import io.netty.channel.nio.NioEventLoopGroup;
 
 /**
  * Created by panlingxiao on 2016/4/11.
+ * Netty 入门:
+ * DiscardServer实现,服务器接收到客户端的信息之后不返回
+ * 直接将收到的消息进行消费，只在服务器端输出。
  */
 public class DiscardServer {
 
