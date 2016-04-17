8ca075f37741e7cabd9df2cef445897d0342fc4e
plx <784580872@qq.com>
2016-04-18 05:13:59 +0800

    Netty的线程源码模型分析

---
+++ netty-eventloop/src/main/java/com/panlingxiao/netty/eventloop/NioEventLoopTest.java
@@ -0,0 +1,14 @@
+package com.panlingxiao.netty.eventloop;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+
+/**
+ * Created by panlingxiao on 2016/4/17.
+ */
+public class NioEventLoopTest {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        System.out.println(boss);
+    }
+}
